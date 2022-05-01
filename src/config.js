32603a558c0f9fe3a9aa9a9084a78e24afd6c0fc
etepes <87408507+etepes@users.noreply.github.com>
2022-05-01 08:50:17 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 
 module.exports = {
-  token: process.env.TOKEN || '', // your discord bot token
-  prefix: process.env.PREFIX || '.', // bot prefix
-  ownerID: process.env.OWNERID || ['959276033683628122', '952560202635427841'], //your discord id
-  SpotifyID: process.env.SPOTIFYID || '', // spotify client id
-  SpotifySecret: process.env.SPOTIFYSECRET || '', // spotify client secret
-  mongourl: process.env.MONGO_URI || 'mongodb+srv://', // MongoDb URL
+  token: process.env.TOKEN || 'ODEzODE3OTcwMDQ4MTA2NTY3.YDU1DA.KBWT5LOdWLvXp3eebn-TRs0RhGM', // your discord bot token
+  prefix: process.env.PREFIX || '?', // bot prefix
+  ownerID: process.env.OWNERID || ['799756254255906816'], //your discord id
+  SpotifyID: process.env.SPOTIFYID || '41c7f9afa63a4e69b91ccf8eebb6a887', // spotify client id
+  SpotifySecret: process.env.SPOTIFYSECRET || '2e31afcbd1754e95951541c3c4c93bce', // spotify client secret
+  mongourl: process.env.MONGO_URI || 'mongodb+srv://ete:papatito@cluster0.zz5be.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // MongoDb URL
   embedColor: process.env.COlOR || '#3366ff', // embed colour
-  logs: process.env.LOGS || '', // Discord channel id 
+  logs: process.env.LOGS || '900170677842243644', // Discord channel id 
   links: {
-    support: process.env.SUPPORT || 'https://discord.gg/ns8CTk9J3e',
-    invite: process.env.INVITE || 'https://discord.gg/ns8CTk9J3e',
-    vote: process.env.VOTE || 'https://discord.gg/ns8CTk9J3e',
+    support: process.env.SUPPORT || 'https://discord.gg/unidrogos',
+    invite: process.env.INVITE || 'https://discord.gg/unidrogos',
+    vote: process.env.VOTE || 'https://discord.gg/unidrogos',
     bg: process.env.BG || 'https://media.discordapp.net/attachments/966675680907657256/967789748699668480/flat-landscape-lake-sunset-deer-wallpaper-preview.jpg'
   },
 
